1ad6d521e1db55f37926c52dfd84f7563147e410
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-05-26 17:09:17 -0300

    Conclui requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   render() {
-    return <p data-testid="ranking-title">Ranking</p>;
+    return (
+      <div>
+        <p data-testid="ranking-title">Ranking</p>
+        <Link to="/">
+          <button type="button" data-testid="btn-go-home">Home</button>
+
+        </Link>
+      </div>
+    );
   }
 }
 
